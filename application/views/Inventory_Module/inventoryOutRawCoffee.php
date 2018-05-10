6934782341c58efc878ccbd86670f3f881c61b8e
jomarilester <2131506@slu.edu.ph>
2018-05-10 21:49:19 +0800

    Change the color of buttons to blue
    Signed-off-by: jomarilester <2131506@slu.edu.ph>

--- application/views/Inventory_Module/inventoryOutRawCoffee.php
+++ application/views/Inventory_Module/inventoryOutRawCoffee.php
@@ -102,6 +102,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			border-top: 3px solid #75DAE2;
 			border-left: 2px solid #75DAE2;
 		}
+
+        .navbar .dropdown-menu li a:hover,
+.navbar .dropdown-menu li a:focus,
+.navbar .dropdown-menu li a:active,
+.navbar.navbar-default .dropdown-menu li a:hover,
+.navbar.navbar-default .dropdown-menu li a:focus,
+.navbar.navbar-default .dropdown-menu li a:active {
+    background-color: #3399ff;
+    color: #FFFFFF;
+    box-shadow: 0 12px 20px -10px rgba(156, 39, 176, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(156, 39, 176, 0.2);
+}
     </style>
 
 <body>
--- application/views/Inventory_Module/inventoryUser.php
+++ application/views/Inventory_Module/inventoryUser.php
@@ -41,7 +41,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     color: white;
 }
 
-
+.navbar .dropdown-menu li a:hover,
+.navbar .dropdown-menu li a:focus,
+.navbar .dropdown-menu li a:active,
+.navbar.navbar-default .dropdown-menu li a:hover,
+.navbar.navbar-default .dropdown-menu li a:focus,
+.navbar.navbar-default .dropdown-menu li a:active {
+    background-color: #3399ff;
+    color: #FFFFFF;
+    box-shadow: 0 12px 20px -10px rgba(156, 39, 176, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(156, 39, 176, 0.2);
+}
 </style>
 
 <body>
