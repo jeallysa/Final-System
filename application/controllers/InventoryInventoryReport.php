4d864fc6fc8f119b719956bc02730b8b33fd83e4
jomarilester <2131506@slu.edu.ph>
2018-05-21 10:02:39 +0800

    fix bug in inventory report

--- application/controllers/InventoryInventoryReport.php
+++ application/controllers/InventoryInventoryReport.php
@@ -26,12 +26,12 @@
         
         public function date_filt(){
             $df = $this->input->post('datefilt');
-            $data['reorder'] = $this->notification_model->reorder();
+            $reorder = $this->notification_model->reorder();
             $data1["coffeein"] = $this->InventoryInventoryReport_Model->get_coffeeinWithP($df);
             $data6["coffeeout"] = $this->InventoryInventoryReport_Model->get_coffeeoutWithP($df);
             $data5["datav"] = $df;
 
-			$this->load->view("Inventory_Module/inventoryInventoryReport", ['data1' => $data1, 'data5' => $data5, 'data6' => $data6]);
+			$this->load->view('Inventory_Module/inventoryInventoryReport', ["reorder" => $reorder, 'data1' => $data1, 'data5' => $data5, 'data6' => $data6]);
         }
 
 	}
