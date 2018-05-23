527d7cad2f7e91583f48aee7e8cac49ba8a3e55b
Averey Maslian <you@example.com>
2018-05-23 14:50:04 +0800

    mods

--- application/views/Sales_Module/salesClientsInfo.php
+++ application/views/Sales_Module/salesClientsInfo.php
@@ -465,8 +465,8 @@
                                     </div>
                                 <?php echo form_close(); ?>
                             </div>
-                        </div>
-                    </div>
+                        </div></div>
+</div></body>
 <script src="<?php echo base_url(); ?>assets/js/jquery-3.2.1.min.js" type="text/javascript"></script>
 <script src="<?php echo base_url(); ?>assets/js/jquery.dataTables.min.js" type="text/javascript"></script>
 <script src="<?php echo base_url(); ?>assets/js/dataTables.bootstrap.min.js" type="text/javascript"></script>
